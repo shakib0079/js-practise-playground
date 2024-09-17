5a7d20b9ac34099139312f1dcd60547738c4a56b
Md. Shaharear Shakib <56974205+shakib0079@users.noreply.github.com>
2024-09-17 14:52:40 +0000

    code change done

--- Practise_Projects/02_BMICalculator/script.js
+++ Practise_Projects/02_BMICalculator/script.js
@@ -3,11 +3,29 @@ const form = document.querySelector('form');
 form.addEventListener('submit', (e) => {
     e.preventDefault()
 
-    const height = document.querySelector('#height').value;
-    const weight = document.querySelector('#weight').value;
+    const height = parseInt(document.querySelector('#height').value);
+    const weight = parseInt(document.querySelector('#weight').value);
     const results = document.querySelector('#results');
 
     const bmi = ((weight / height / height) * 10000).toFixed(2);
 
-    results.innerHTML = `<span>${bmi}</span>`
+    if(height < 0 || isNaN(height) || height == ''){
+        results.innerHTML = results.innerHTML = `<span>Please enter a valid height</span>`
+    }else if(weight < 0 || isNaN(weight) || weight == ''){
+        results.innerHTML = results.innerHTML = `<span>Please enter a valid weight</span>`
+    }else{
+        results.innerHTML = `<span>Your BMI is ${bmi}</span>`
+    }
+
+    const paragraph = document.querySelectorAll('.overview');
+    if(bmi < 18.6){
+        paragraph[0].style.color = 'yellow'
+    }else if (bmi > 18.6 && bmi < 24.9){
+        paragraph[1].style.color = 'green'
+    }else if(bmi > 24.9){
+        paragraph[2].style.color = 'red'
+    }
+    // paragraph.forEach((e) => {
+    //     console.log(e);
+    // })
 })
