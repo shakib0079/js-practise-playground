dfd106de4b1b8fbcfecb3f7b654a19a0928c5695
Md. Shaharear Shakib <56974205+shakib0079@users.noreply.github.com>
2024-09-18 09:57:10 +0000

    New code commit

--- Practise_Projects/04_guessTheNumber/script.js
+++ Practise_Projects/04_guessTheNumber/script.js
@@ -1,7 +1,15 @@
-const submitButton = document.querySelector('#subt');
+// Random Number
+//10 Attempt
+//Guess Remaining
+//Low or High
+//reset game
+//start new game
 
-submitButton.addEventListener('click', (e) => {
-    e.preventDefault();
-    const inputValue = parseInt(document.querySelector('.guessField').value);
-    console.log(inputValue);
-})
+// 1. Generate Random Number
+// 2. Take Input from User
+// 3. Validating Input
+// 4. Comparing Random Number and User Input
+// 5. Display the result
+// 6. Remaining chances
+// 7. End The Game
+// 8. Restart The Game
