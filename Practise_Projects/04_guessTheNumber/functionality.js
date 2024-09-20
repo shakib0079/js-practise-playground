4c45b30e4ca475363938764ed55082adc92e1eb2
Md. Shaharear Shakib <56974205+shakib0079@users.noreply.github.com>
2024-09-20 16:50:24 +0000

    Game done

--- Practise_Projects/04_guessTheNumber/functionality.js
+++ Practise_Projects/04_guessTheNumber/functionality.js
@@ -6,13 +6,17 @@ const starNewGame = document.querySelector('.newGame')
 const displayResult = document.querySelector('.lowOrHi')
 const resultDisplay = document.querySelector('.resultParas')
 
+starNewGame.style.display = "none"
+
+console.log(starNewGame)
+
 let randomNumber = Math.floor(((Math.random() * 100) + 1))
 //console.log(randomNumber);
 
 const p = document.createElement('p')
 let playGame =true;
 let prevGuess = [];
-let chances = 10;
+let chances = 1;
 
 submitButton.addEventListener('click', (e) => {
     e.preventDefault();
@@ -22,7 +26,7 @@ submitButton.addEventListener('click', (e) => {
         userGuessValidation(userGuess);    
     }
     
-
+    console.log(randomNumber)
 })
 
 function userGuessValidation(guess){
@@ -33,15 +37,15 @@ function userGuessValidation(guess){
     }else if(isNaN(guess)){
         displayMessage("Please Enter a Number in between 1 to 100")
     }else{
-        if(chances === 0){
+        if(chances === 10){
             displayMessage("Your Game Is Over")
+            endGame()
         }else{
             compareWithRandomNumber(guess);
             // console.log(userInput.value);
+            displayCheck(guess);
             prevGuess.push(guess);
-            userInput.value = '';
-            chances--;
-            console.log(chances);
+            
         }
         
     }
@@ -54,11 +58,49 @@ function compareWithRandomNumber(guess){
         displayMessage("you guessed Low!")
     }else if(guess === randomNumber){
         displayMessage("Congratulation! You guessed Right!")
+        endGame()
     }
 }
 
+function displayCheck(guess){
+    userInput.value = '';
+    chances++;
+    console.log(chances);
+    prevGuesses.innerHTML += `${guess}  `
+}
+
 function displayMessage(message){
-    p.innerHTML = `<h3>${message}</h3>`
-    resultDisplay.appendChild(p);
+    // p.innerHTML = `<h3>${message}</h3>`
+    // resultDisplay.appendChild(p);
+    displayResult.innerHTML = `<h3>${message}</h3>`
     console.log(p);
+
+    console.log(typeof(chances));
+    
+    remainingChance.innerHTML = `${10 - chances}`;
+}
+
+function endGame(){
+    userInput.setAttribute("disabled", "")
+    starNewGame.style.display = "block"
+    playGame = false
+
+    starNewGame.addEventListener('click', (e) => {
+        e.preventDefault();
+        startGameAgain()
+    })
+}
+
+function startGameAgain(){
+    console.log("the game start again!")
+    starNewGame.style.display = "none"
+
+    userInput.value = '';
+    prevGuesses.innerHTML = "";
+    userInput.removeAttribute("disabled", "")
+    displayResult.innerHTML = ''
+    chances = 1;
+    prevGuess = [];
+    remainingChance.innerHTML = "";
+    playGame = true;
 }
